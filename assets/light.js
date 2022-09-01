e6e8faf67c474d65850bf229be7de460aa019ce2
Jaden Park <78674944+VsBJams@users.noreply.github.com>
2022-09-01 11:22:02 -0400

    Reverting dark mode change

--- assets/light.js
+++ assets/light.js
@@ -1,24 +1,12 @@
 // Light and dark mode
 var icon = document.getElementById("icon");
 
-let localData = localStorage.getItem("theme");
-
-if (localData == "dark") {
-    icon.src = "images/light&dark/sun.png";
-    document.body.classList.remove("light-theme");
-} else if (localData == "light") {
-    icon.src = "images/light&dark/moon.png";
-    document.body.classList.add("light-theme");
-}
-
 icon.onclick = function () {
     document.body.classList.toggle("light-theme");
     if (document.body.classList.contains("light-theme")) {
         icon.src = "images/light&dark/moon.png";
-        localStorage.setItem("theme", "light");
     } else {
         icon.src = "images/light&dark/sun.png";
-        localStorage.setItem("theme", "dark");
     }
 }
 
