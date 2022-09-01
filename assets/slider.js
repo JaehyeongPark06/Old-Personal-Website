7ef85f3e010b64ed45292b5b2fad551ee61c5ac3
Jaden Park <78674944+VsBJams@users.noreply.github.com>
2022-09-01 16:40:46 -0400

    Project carousel and image changes

---
+++ assets/slider.js
@@ -0,0 +1,37 @@
+const slide = document.querySelector('.slider-wrapper');
+
+// TINY SLIDER
+let projects_slider = tns({
+    container: ".my-slider",
+    animateIn: "jello",
+    animateOut: "rollOut",
+    slideBy: 1,
+    speed: 400,
+    nav: false,
+    swipeAngle: false,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    controlsContainer: "#project-controls",
+    prevButton: ".previous",
+    nextButton: ".next",
+    responsive: {
+        1280: {
+            items: 3,
+            gutter: 20
+        },
+        1024: {
+            items: 2,
+            gutter: 20
+        },
+        810: {
+            items: 2,
+            gutter: 20
+        },
+        768: {
+            items: 2
+        },
+        480: {
+            items: 1
+        }
+    }
+});
