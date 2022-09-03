d432ff5924d7050cf577adb9e8f3702de43fcd32
Jaden Park <78674944+VsBJams@users.noreply.github.com>
2022-09-03 18:39:19 -0400

    Light mode reverse change

--- assets/light.js
+++ assets/light.js
@@ -1,12 +1,28 @@
 // Light and dark mode
 var icon = document.getElementById("icon");
 
+if (localStorage.getItem("theme") == null) {
+    localStorage.setItem("theme", "dark");
+}
+
+let localData = localStorage.getItem("theme");
+
+if (localData === "light") {
+    icon.src = "images/light&dark/moon.png";
+    document.body.classList.remove("dark-theme");
+} else if (localData === "dark") {
+    icon.src = "images/light&dark/sun.png";
+    document.body.classList.toggle("dark-theme");
+}
+
 icon.onclick = function () {
-    document.body.classList.toggle("light-theme");
-    if (document.body.classList.contains("light-theme")) {
-        icon.src = "images/light&dark/moon.png";
-    } else {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
         icon.src = "images/light&dark/sun.png";
+        localStorage.setItem("theme", "dark")
+    } else {
+        icon.src = "images/light&dark/moon.png";
+        localStorage.setItem("theme", "light")
     }
 }
 
